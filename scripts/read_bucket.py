96317246543ee851cda2e00f34422c0c721e9e17
astroAycha <aycha.tammour@gmail.com>
2026-04-06 10:29:05 -0400

    Added a funtion to read the forecasts tables from S3

--- scripts/read_bucket.py
+++ scripts/read_bucket.py
@@ -42,3 +42,50 @@ class DataReader:
         print(f"Data loaded: {results_df.shape}")
 
         return results_df
+    
+    def read_forecasts(self, 
+                       exp_name: str,
+                       aoi_name: str,
+                       forecast_date: str) -> pd.DataFrame:
+        """Read the forecast data from S3 as a pandas dataframe
+        Parameters:
+        ----------
+        exp_name: str
+            The name of the experiment to filter the data by.
+        aoi_name: str
+            The name of the area of interest to filter the data by.
+        forecast_date: str
+            The date of the forecast to filter the data by.
+        Returns:
+        -------
+        pd.DataFrame
+            The DataFrame containing the loaded forecast data.
+        """
+
+        if not forecast_date == 'latest':
+            query = """
+            SELECT *
+            FROM read_parquet(? || ? || '/**/*.parquet')
+            WHERE aoi_name = ?
+            AND forecast_date = ?
+            """
+            params = [f's3://{self.bucket_name}/', 'forecasts', exp_name, aoi_name, forecast_date]
+            results_df = self.conn.execute(query, params).df()
+            print(f"Forecast data loaded: {results_df.shape}")
+
+        else:
+            query = """
+            WITH latest AS (
+                SELECT MAX(forecast_date) AS latest_date
+                FROM read_parquet(? || ? || '/**/*.parquet')
+            )
+            SELECT *
+            FROM read_parquet(? || ? || '/**/*.parquet') AS forecasts
+            JOIN latest
+            ON forecasts.forecast_date = latest.latest_date
+            """
+            params = [f's3://{self.bucket_name}/', 'forecasts', f's3://{self.bucket_name}/', 'forecasts']
+            results_df = self.conn.execute(query, params).df()
+            print(f"Forecast data loaded: {results_df.shape}")
+
+        return results_df
